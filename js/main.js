b8eaca7d30e37f0c244d7d29939b40e3c7f3a75d
sbrvls <sabirovvaal@mail.rudit>
2023-09-08 08:41:58 +0300

    123

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 let price_tovar = prompt("Введите стоимость товара");
 let money = prompt('Введите количество денег клиента');
 if (price_tovar == money){
-    console.log('Покупка совершена.');
-} if (price_tovar > money){
+    alert('Покупка совершена.');
+} else if (price_tovar > money){
     let ost = price_tovar - money;
-    console.log('Недостаточно ' + ost + 'рублей');
-} if (price_tovar < money){
-    let sdacha = money - price_tovar;
-    console.log('Сдача ' + sdacha +  ' рублей');
+    alert('Недостаточно ' + ost + ' рублей');
+} else if (price_tovar < money){ 
+    let ost = money - price_tovar;
+    alert('Сдача ' + ost +  ' рублей');
 }
